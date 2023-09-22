d80a1fed943ff6b01cf1f25ab3e3ffe86ef8d6f4
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-09-21 21:40:45 -0400

    Add files via upload

---
+++ Cap.3.programas/programa 3.15.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+/* Calculo de p. EL programa obtiene el valor de P aplicando una serie determinada.
+I, B, C: variables de tipo real doble precision.*/
+void main (void)
+{
+    int I = 1, B = 0, C;
+    double RES;
+    RES = 4.0/ I;
+    C = 1;
+    while((fabs(3.1415 - RES)) > 0.0005)
+    {
+        I += 2;
+        if (B)
+        {
+            RES += (double) (4.0 / I);
+            B = 0;
+        }
+        else
+        {
+            RES = (double) (4.0 / I);
+            B = 1;
+        }
+        C++;
+    }
+    printf("\n el numero de terminos: %d", C);
+}
+
