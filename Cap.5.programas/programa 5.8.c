d942184c9742e828b77f5bee5916bcf94a46e19d
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-10-10 11:11:02 -0400

    Add files via upload

---
+++ Cap.5.programas/programa 5.8.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Primos.
+El programa almacena en un
+arreglo unidimensional
+los primeros 100 numeros
+primos. */
+
+const int TAM = 10;
+
+void Imprime(int, int);
+void Primo (int, int *);
+
+void main(void)
+{
+    int P[TAM] = {1,2};
+    int FLA, J = 2, PRI = 3;
+    while (J<= TAM)
+    {
+        FLA = 1;
+        Primo(PRI, &FLA);
+        if (FLA)
+        {
+            P[J] = PRI;
+            J++;
+        }
+        PRI += 2;
+    }
+    Imprime(P, TAM);
+}
+void Primo (int A, int *B)
+{
+    int DI = 3;
+    while (*B && (DI < (A/ 2)))
+    {
+        if ((A % DI)== 0)
+            *B = 0;
+        DI++;
+    }
+}
+void Imprime(int Primos[], int T)
+{
+    int I;
+    for (I=0; I<T; I++)
+        printf("\nPrimos[%d]: %d", I , Primos[I]);
+}
