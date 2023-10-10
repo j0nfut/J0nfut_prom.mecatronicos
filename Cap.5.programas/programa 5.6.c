9c9dde361712ed54dfa835526169ccfe6f862381
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-10-10 11:09:42 -0400

    Add files via upload

---
+++ Cap.5.programas/programa 5.6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Suma-cuadrados.
+El programa calcula la suma del cuadrado
+de los elementos de un arreglo
+unidimensional de 100 elementos de tipo real. */
+
+const int MAX = 10;
+void Lectura (float *, int);
+double Suma (float *, int);
+
+void main (void)
+{
+    float VEC[MAX];
+    double RES;
+    Lectura (VEC, MAX);
+    RES = Suma(VEC, MAX);
+    printf("\n\n Suma del arreglo: %.2lf", RES);
+}
+void Lectura (float A[], int T)
+{
+    int I;
+    for (I = 0; I<T; I++)
+    {
+        printf("Ingrese el elemento %d: ", I+1);
+        scanf("%f", &A[I]);
+    }
+}
+double Suma(float A[], int T)
+{
+    int I;
+    double AUX = 0.0;
+    for (I= 0; I<T; I++)
+        AUX += pow(A[I], 2);
+    return (AUX);
+
+}
