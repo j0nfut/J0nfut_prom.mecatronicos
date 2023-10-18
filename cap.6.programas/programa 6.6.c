cada218db7b8d1ea25d45f821478879ac565de0f
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-10-18 16:51:08 -0400

    Add files via upload

---
+++ cap.6.programas/programa 6.6.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Intercambia.
+El programa intercambia las columnas de un arreglo bidimensional. Los
+elementos de la primera columna se intercambian con los de la ultima,
+los de la segunda con los de la penultima, y así sucesivamente. */
+
+const int MAX = 20;
+
+void Lectura(float [][MAX], int, int);
+void Intercambia(float [][MAX], int, int);
+void Imprime(float [][MAX], int, int);
+
+void main(void)
+{
+    int F, C;
+    float MAT[MAX][MAX];
+    do
+    {
+        printf("Ingrese el numero de filas: ");
+        scanf("%d", &F);
+    }
+    while (F > MAX || F < 1);
+
+    do
+    {
+        printf("Ingrese el numero de columnas: ");
+        scanf("%d", &C);
+    }
+    while (C > MAX || C < 1);
+
+    Lectura(MAT, F, C);
+    Intercambia(MAT, F, C);
+    Imprime(MAT, F, C);
+}
+
+void Lectura(float A[][MAX], int F, int C)
+{
+    int I, J;
+    for (I = 0; I < F; I++)
+        for (J = 0; J < C; J++)
+        {
+            printf("Ingrese el elemento %d %d: ", I + 1, J + 1);
+            scanf("%f", &A[I][J]);
+        }
+}
+
+void Intercambia(float A[][MAX], int F, int C)
+{
+    int I, J;
+    float AUX;
+
+    for (I = 0; I < (C / 2); I++)
+        for (J = 0; J < F; J++)
+        {
+            AUX = A[J][I];
+            A[J][I] = A[J][C - I - 1];
+            A[J][C - I - 1] = AUX;
+        }
+}
+
+void Imprime(float A[][MAX], int F, int C)
+{
+    int I, J;
+    for (I = 0; I < F; I++)
+        for (J = 0; J < C; J++)
+            printf("Elemento %d %d: %5.2f\n", I + 1, J + 1, A[I][J]);
+}
