9f4dd6e5f293e8fdf8511b897860901bf3f46b63
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-10-26 09:40:20 -0400

    Add files via upload

---
+++ cap.7.programas/programa 7.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Funciones para el manejo de caracteres de la biblioteca stdio.h */
+void main(void)
+{
+    char p1, p2, p3 = '$';
+
+    printf("\nIngrese un caracter: ");
+    p1 = getchar();
+    putchar(p1);
+    printf("\n");
+    fflush(stdin);
+
+    printf("\nEl caracter p3 es: ");
+    putchar(p3);
+
+    printf("\n");
+    printf("\nIngrese otro caracter: ");
+    fflush(stdin);
+    scanf("%c", &p2);
+
+    printf("%c", p2);
+
+}
