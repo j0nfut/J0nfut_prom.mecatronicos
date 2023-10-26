0cb175150c4a25daa9c6b6378af1fe2b903feda6
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-10-26 11:03:07 -0400

    Add files via upload

---
+++ cap.7.programas/programa 7.19.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Cuenta palabras.
+El programa calcula el numero de palabras que
+hay en la cadena de caracteres. */
+
+int cuentap(char *);
+
+void main(void)
+{
+    int i;
+    char fra[50];
+
+    printf("\nIngrese la linea de texto: ");
+    gets(fra);
+    strcat(fra, " ");
+    i = cuentap(fra);
+    printf("\nLa linea de texto tiene %d palabras", i);
+}
+
+int cuentap(char *cad)
+{
+
+    char *cad0 = "";
+    int i = 0;
+    cad0 = strstr(cad, " ");
+
+    while (strcmp(cad, " "))
+    {
+        strcpy(cad, cad0);
+        i++;
+        cad0 = strstr(cad + 1, " ");
+
+    }
+
+    return i;
+}
