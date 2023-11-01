cb390614130acf72513e6f8a0de0c1c5329cf73a
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-11-01 18:42:16 -0400

    Add files via upload

---
+++ cap.9.programa/programa 9.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Archivos y caracteres. El programa lee caracteres de un archivo.*/
+void main(void)
+{
+    char p1;
+    FILE *ar;
+    if ((ar = fopen("arc.txt", "r")) != NULL)
+    {
+        while (!feof(ar))
+        {
+            p1 = fgetc(ar);
+            putchar(p1);
+
+        }
+        fclose(ar);
+
+    }
+    else
+        printf("No se puede abrir el archivo");
+}
