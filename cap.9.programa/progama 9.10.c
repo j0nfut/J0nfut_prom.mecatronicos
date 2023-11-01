f02574eaf25522acfac71be0fd2ae1d8e4abd78e
j0nfut <144465350+j0nfut@users.noreply.github.com>
2023-11-01 19:56:04 -0400

    Add files via upload

---
+++ cap.9.programa/progama 9.10.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Alumnos. El programa pregunta al usario el numero de registro que desea
+modificar, obtiene el nuevo promedio del alumno
+y modifica tanto el registro como el archivo correspondiente.*/
+
+typedef struct {
+    int matricula;
+    char nombre[20];
+    int carrera;
+    float promedio;
+} alumno;
+
+void lee(FILE *);
+
+void main(void)
+{
+    FILE *ar;
+
+    if ((ar = fopen("ad1.dat", "r")) != NULL)
+        lee(ar);
+    else
+        printf("El archivo no se puede abrir");
+
+    fclose(ar);
+}
+
+void lee(FILE *ap)
+{
+    alumno alu;
+
+    fread(&alu, sizeof(alumno), 1, ap);
+
+    while (!feof(ap))
+    {
+        printf("\nMatricula: %d", alu.matricula);
+        printf("\tCarrera: %d", alu.carrera);
+        printf("\tPromedio: %f\t", alu.promedio);
+        puts(alu.nombre);
+
+        fread(&alu, sizeof(alumno), 1, ap);
+    }
+}
